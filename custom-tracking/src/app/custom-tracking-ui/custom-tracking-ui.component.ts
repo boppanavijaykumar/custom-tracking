a187f97301b8dad0e21ec2a4a37bb47a53908e6a
Shishika <Shishika.Bethireddy@neudesic.com>
2018-08-02 10:39:27 +0530

    created a component for ui

---
+++ custom-tracking/src/app/custom-tracking-ui/custom-tracking-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-tracking-ui',
+  templateUrl: './custom-tracking-ui.component.html',
+  styleUrls: ['./custom-tracking-ui.component.css']
+})
+export class CustomTrackingUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
