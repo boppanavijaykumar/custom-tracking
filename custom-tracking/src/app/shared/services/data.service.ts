a14adc5ad295f50f34c7da2fbe8a00a434cd52d7
Shishika <Shishika.Bethireddy@neudesic.com>
2018-08-09 18:50:09 +0530

    optimized the code in data service

--- custom-tracking/src/app/shared/services/data.service.ts
+++ custom-tracking/src/app/shared/services/data.service.ts
@@ -4,46 +4,77 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
+  param: string;
   constructor(public trackingDetailsService: TrackingDetailsService) {}
 
   public addScript(trackingDetails: ITrackingDetails) {
-    this.addHeaderScript(trackingDetails.head);
-    this.addBodyScript(trackingDetails.body);
+    this.scriptAdder(trackingDetails.head, true);
+    this.scriptAdder(trackingDetails.body, false);
   }
+
   private parseHTML(html) {
     const template = document.createElement('template');
     template.innerHTML = html;
     return template.content.cloneNode(true);
   }
-  private addHeaderScript(headerScript: string): void {
-    const regExp = RegExp('<script>');
-    let newScript;
-    if (regExp.test(headerScript)) {
-      const target = document.getElementsByTagName('head')[0];
-      const inlineScript = this.parseHTML(headerScript);
-      target.appendChild(inlineScript);
-    } else {
-      const target = document.getElementsByTagName('head')[0];
-      newScript = document.createElement('script');
-      const inlineScript = document.createTextNode(headerScript);
-      newScript.appendChild(inlineScript);
-      target.appendChild(newScript);
-    }
-  }
 
-  private addBodyScript(bodyScript: string): void {
-    const regExp = RegExp('<script>');
-    let newScript;
-    if (regExp.test(bodyScript)) {
-      const target = document.getElementsByTagName('body')[0];
-      const inlineScript = this.parseHTML(bodyScript);
-      target.appendChild(inlineScript);
+  public scriptAdder(script: string, isHead: boolean) {
+
+    if (isHead) {
+       this.param = 'head';
     } else {
-      const target = document.getElementsByTagName('body')[0];
-      newScript = document.createElement('script');
-      const inlineScript = document.createTextNode(bodyScript);
-      newScript.appendChild(inlineScript);
-      target.appendChild(newScript);
+      this.param = 'body';
     }
-  }
+    const regExp = RegExp('<script>');
+      let newScript;
+      if (regExp.test(script)) {
+        const target = document.getElementsByTagName(this.param)[0];
+        const inlineScript = this.parseHTML(script);
+        target.appendChild(inlineScript);
+      } else {
+        const target = document.getElementsByTagName(this.param)[0];
+        newScript = document.createElement('script');
+        const inlineScript = document.createTextNode(script);
+        newScript.appendChild(inlineScript);
+        target.appendChild(newScript);
+      }
+    }
+
+
+  // public addScript(trackingDetails: ITrackingDetails) {
+  //   this.addHeaderScript(trackingDetails.head);
+  //   this.addBodyScript(trackingDetails.body);
+  // }
+
+  // private addHeaderScript(headerScript: string): void {
+  //   const regExp = RegExp('<script>');
+  //   let newScript;
+  //   if (regExp.test(headerScript)) {
+  //     const target = document.getElementsByTagName('head')[0];
+  //     const inlineScript = this.parseHTML(headerScript);
+  //     target.appendChild(inlineScript);
+  //   } else {
+  //     const target = document.getElementsByTagName('head')[0];
+  //     newScript = document.createElement('script');
+  //     const inlineScript = document.createTextNode(headerScript);
+  //     newScript.appendChild(inlineScript);
+  //     target.appendChild(newScript);
+  //   }
+  // }
+
+  // private addBodyScript(bodyScript: string): void {
+  //   const regExp = RegExp('<script>');
+  //   let newScript;
+  //   if (regExp.test(bodyScript)) {
+  //     const target = document.getElementsByTagName('body')[0];
+  //     const inlineScript = this.parseHTML(bodyScript);
+  //     target.appendChild(inlineScript);
+  //   } else {
+  //     const target = document.getElementsByTagName('body')[0];
+  //     newScript = document.createElement('script');
+  //     const inlineScript = document.createTextNode(bodyScript);
+  //     newScript.appendChild(inlineScript);
+  //     target.appendChild(newScript);
+  //   }
+  // }
 }
